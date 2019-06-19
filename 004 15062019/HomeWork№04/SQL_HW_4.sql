15efe17cf041d2f2e56eae4bf03a4f8db365a296
Anatolii.Zamlynnyi <Anatolii.Zamlynnyi@gmail.com>
2019-06-19 20:13:15 +0300

    004Full

--- 004 15062019/HomeWork№04/SQL_HW_4.sql
+++ 004 15062019/HomeWork№04/SQL_HW_4.sql
@@ -90,18 +90,47 @@ SET @i+=1
 END
 
 DECLARE @rez INT;
-EXEC FactorNumber 10, @rez OUTPUT
+EXEC FactorNumber 8, @rez OUTPUT
 PRINT ('Factorial number = '+convert(NVARCHAR(MAX),@rez))
 
 
 --10. Написать хранимую процедуру, которая позволяет увеличить дату издательства каждой книги, которая соответствует шаблону на 2 года. 
 --Шаблон передается в качестве параметра в процедуру.
+GO
+CREATE PROCEDURE NewDate(@author NVARCHAR(MAX), @date DATE) AS
+UPDATE Book
+SET Book.PublishDate=
+(CASE
+WHEN Author.AuthorName=@author AND Book.PublishDate<@date THEN DATEADD(YEAR,2,Book.PublishDate)
+ELSE Book.PublishDate
+END)
+FROM Book JOIN Author ON Book.AuthorId=Author.Id;
+
+EXEC NewDate 'Zagrebelnyj Pavel', '20180101'
 
 --11. Написать хранимую процедуру с параметрами, определяющими диапазон дат выпуска книг. Процедура позволяет обновить данные 
 --о тираже выпуска книг по следующим условиям:
---	- Если дата выпуска книги находится в определенном диапазоне, тогда тираж нужно увеличить в два раза, 
---	  а цену за единицу увеличить на 20%;
+--	- Если дата выпуска книги находится в определенном диапазоне, тогда тираж нужно увеличить в два раза, а цену за единицу увеличить на 20%;
 --	- Если дата выпуска книги не входит в диапазон, тогда тираж оставить без изменений.
-
---	!!!Предусмотреть вывод на экран соответствующих со общений об ошибке, если передаваемые даты одинаковые, или конечная дата 
+--	!!!Предусмотреть вывод на экран соответствующих сообщений об ошибке, если передаваемые даты одинаковые, или конечная дата 
 --промежутка меньше начала, или же начальная больше текущей даты.
+GO
+CREATE PROCEDURE RangeDate(@date1 DATE, @date2 DATE) AS
+IF(@date1=@date2)
+PRINT ('Date 1 = Date2!')
+IF(@date2<@date1)
+PRINT ('Date 2 < Date1!')
+IF(@date1>=GETDATE())
+PRINT ('Date 1 >= The current date!')
+ELSE
+BEGIN
+	IF EXISTS (SELECT *FROM Book WHERE Book.PublishDate BETWEEN @date1 AND @date2)
+	BEGIN
+		UPDATE Incomes SET Incomes.Amount=Incomes.Amount*2 WHERE Incomes.BookId IN (SELECT Id FROM Book WHERE Book.PublishDate BETWEEN @date1 AND @date2)
+		UPDATE Book SET Book.Price=Book.Price*1.2 WHERE Book.Id IN (SELECT Id FROM Book)
+	END
+END
+
+SELECT *FROM Book JOIN Incomes ON Incomes.BookId=Book.Id
+EXEC RangeDate '20030101', '20110101'
+SELECT *FROM Book JOIN Incomes ON Incomes.BookId=Book.Id
